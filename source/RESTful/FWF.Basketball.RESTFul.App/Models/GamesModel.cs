5faee9bdb1fe4ef87bc2c0d3b8e23e973b399fec
Jon Banta <jon.banta@roundpointmortgage.com>
2018-10-22 12:47:11 -0400

    Add RESTful App

---
+++ source/RESTful/FWF.Basketball.RESTFul.App/Models/GamesModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FWF.Basketball.RESTFul.App.Models
+{
+    public class GamesModel
+    {
+
+        public GamesModel()
+        {
+            this.Games = new List<GameDetail>();
+        }
+
+        public List<GameDetail> Games
+        {
+            get; set;
+        }
+    }
+}
