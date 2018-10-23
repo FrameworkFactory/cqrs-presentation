75d3dd682d3b33e7ef1eb0b648c6c26ae1e96455
Jon Banta <jon.banta@roundpointmortgage.com>
2018-10-23 09:30:01 -0400

    Ensure web app is updated with each score and game clock change

--- source/CQRS/FWF.Basketball.CQRS.App/src/storage/gameStorage.js
+++ source/CQRS/FWF.Basketball.CQRS.App/src/storage/gameStorage.js
@@ -1,122 +1,130 @@
 
 
 const gameStorage = {
-    namespaced: true,
+  namespaced: true,
+
+  state: {
+      message: "",
+      games: [],
+      players: [],
+      scores: []
+    },
   
-    state: {
-        message: "",
-        games: [],
-        players: [],
-        scores: []
+    getters: {
+      getGameById: state => (id) => {
+        return state.games.find(x => x.id === id)
       },
-    
-      getters: {
-        getGameById: state => (id) => {
-          return state.games.find(x => x.id === id)
-        },
-        getPlayerById: state => (id) => {
-          return state.players.find(x => x.id === id)
+      getPlayerById: state => (id) => {
+        return state.players.find(x => x.id === id)
+      }
+    },
+   
+    mutations:{
+      addOrUpdateGameDetail(state, game) {
+
+        var localGame = state.games.find(function (x) { return x.id === game.id });
+
+        if (!localGame) {
+          game.awayScore = 0;
+          game.homeScore = 0;
+          state.games.push(game);
         }
-      },
-     
-      mutations:{
-        addOrUpdateGameDetail(state, game) {
+        else {
+          localGame.name = game.name;
+          localGame.quarter = game.quarter;
+          localGame.gameClock = game.gameClock;
 
-          var localGame = state.games.find(function (x) { return x.id === game.id });
-
-          if (!localGame) {
-            state.games.push(game);
+          if (game.awayScore) {
+              localGame.awayScore = game.awayScore;
           }
-          else {
-            localGame.name = game.name;
-            localGame.quarter = game.quarter;
-            localGame.gameClock = game.gameClock;
-            localGame.awayScore = game.awayScore;
+
+          if (game.homeScore) {
             localGame.homeScore = game.homeScore;
-          }
-        },
-
-        addOrUpdatePlayerDetail(state, player) {
-
-          var localPlayer = state.players.find(function (x) { return x.id === player.id });
-
-          if (!localPlayer) {
-            state.players.push(player);
-          }
-          else {
-            localPlayer.totalPoints = player.totalPoints;
-          }
-        },
-
-        addScore(state, score) {
-/*
-          var localGame = state.games.find(function (x) { return x.id === score.gameId });
-
-          if (localGame) {
-
-            // Determine if the score increments the home or away team
-            if (score.teamId == localGame.awayTeamId) {
-              localGame.awayScore = localGame.awayScore + score.points;
-            }
-            if (score.teamId == localGame.homeTeamId) {
-              localGame.homeScore = localGame.homeScore + score.points;
-            }
-          }
-*/
-          state.scores.push(score);
+          }            
         }
       },
 
-      actions: {
-        gameDetailChangeEvent: function(context, message) {
+      addOrUpdatePlayerDetail(state, player) {
 
-          var game = {
+        var localPlayer = state.players.find(function (x) { return x.id === player.id });
+
+        if (!localPlayer) {
+          state.players.push(player);
+        }
+        else {
+          localPlayer.totalPoints = player.totalPoints;
+        }
+      },
+
+      addScore(state, score) {
+        state.scores.push(score);
+      },
+
+    },
+
+    actions: {
+      gameDetailChangeEvent: function(context, message) {
+
+        var game = {
+          id: message.id,
+          name: message.name,
+          quarter: message.quarter,
+          gameClock: message.gameClock,
+          awayTeamId : message.awayTeamId,
+          awayScore: message.awayScore,
+          homeTeamId : message.homeTeamId,
+          homeScore: message.homeScore
+        };
+
+        context.commit('addOrUpdateGameDetail', game);
+      },
+
+      gameClockChangeEvent: function(context, message) {
+
+        var game = {
+          id: message.gameId,
+          name: message.gameName,
+          quarter: message.quarter,
+          gameClock: message.gameClock,
+          awayTeamId : message.awayTeamId,
+          homeTeamId : message.homeTeamId,
+        };
+
+        context.commit('addOrUpdateGameDetail', game);
+      },
+
+      playerDetailChangeEvent: function(context, message) {
+
+        var player = {
             id: message.id,
             name: message.name,
-            quarter: message.quarter,
-            gameClock: message.gameClock,
-            awayTeamId : message.awayTeamId,
-            awayScore: message.awayScore,
-            homeTeamId : message.homeTeamId,
-            homeScore: message.homeScore
-          };
-
-          context.commit('addOrUpdateGameDetail', game);
-        },
-
-        playerDetailChangeEvent: function(context, message) {
-
-          var player = {
-              id: message.id,
-              name: message.name,
-              teamId: message.teamId,
-              position: message.position,
-              totalPoints: message.totalPoints 
-          };
-          context.commit('addOrUpdatePlayerDetail', player);
-        },
-
-        scoreChangeEvent: function(context, message) {
-
-          var score = {
-            gameId: message.gameId,
             teamId: message.teamId,
-            playerId: message.playerId,
-            points: message.points
-          };
-          context.commit('addScore', score);
-        },
+            position: message.position,
+            totalPoints: message.totalPoints 
+        };
+        context.commit('addOrUpdatePlayerDetail', player);
+      },
 
-      }
+      scoreChangeEvent: function(context, message) {
 
-      /*
-      actions: {
-        sendMessage: function(context, message) {
-          Vue.prototype.$socket.send(message)
-        }
+        var score = {
+          gameId: message.gameId,
+          teamId: message.teamId,
+          playerId: message.playerId,
+          points: message.points
+        };
+        context.commit('addScore', score);
+      },
+
+    }
+
+    /*
+    actions: {
+      sendMessage: function(context, message) {
+        Vue.prototype.$socket.send(message)
       }
-      */
-  }
-  
-  export default gameStorage
-  
+    }
+    */
+}
+
+export default gameStorage
